10ab1b82cca9bc78821017a4d1efa082e5dd6f01
RacovitaMadalina <root@hostname1.localdomain>
2019-03-11 14:38:22 +0200

    another fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -62,7 +62,7 @@
 
     		operation_status= replace_key_value_pair "SELINUX" "=" "disabled" /etc/selinux/config
    	 	operation_status=$operation_status && setenforce 0
-    		operation_status= replace_key_value_pair "SELINUX" "=" "enforcing" /etc/selinux/conf
+    		operation_status= replace_key_value_pair "SELINUX" "=" "enforcing" /etc/selinux/config
    		return $operation_status
 	}
 
