bca222b3c55e62d4525c52b966ee4ac9477bf332
RacovitaMadalina <root@hostname1.localdomain>
2019-03-11 12:49:56 +0200

    Fixed bug

--- other_service_functionalities.sh
+++ other_service_functionalities.sh
@@ -3,12 +3,12 @@
 # setting the hostname for the virtual machine
 # sudo su
 
-#ifup enp0s8
-#ifup enp0s3
+ifup enp0s8
+ifup enp0s3
 
 hostname="localhost.localdomain"
 
-base_name="madalina"
+base_name="hostname1"
 desired_hostname="$base_name.localdomain"
 echo $desired_hostname
 
@@ -32,7 +32,7 @@ mkdir ~/$new_dirname
 cd ~/$new_dirname
 
 #connected to git via ssh
-eval `ssh-agent` | ssh-add /etc/ssh/id_rsa_git | git clone git@github.com:RacovitaMadalina/MLMOS.git
+ssh-agent bash -c 'ssh-add /etc/ssh/id_rsa_git; git clone git@github.com:RacovitaMadalina/MLMOS.git'
 
 #executing the script bootstrap.sh
-bash /root/$new_dirname/bootstrap.sh
+bash /root/$new_dirname/MLMOS/bootstrap.sh
